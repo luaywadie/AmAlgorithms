f7800793912fe511974c70c4db5a72e447acf102
Luay Wadie <41401210+luaywadie@users.noreply.github.com>
2020-10-09 22:13:50 -0400

    Updated Homepage elements

--- visualizations-app/src/Components/MainPage.js
+++ visualizations-app/src/Components/MainPage.js
@@ -9,6 +9,11 @@ const MainPage = (props) => {
         <span style={{color:"#51a6fc"}}> Am</span>
         <span style={{color:"#8787fe"}}>Algorithms</span>
       </h1>
+      <div className="background">
+        <img src={process.env.PUBLIC_URL + '/assets/imgs/home_page_background1.png'} />
+        <img src={process.env.PUBLIC_URL + '/assets/imgs/home_page_background2.png'} />
+        <img src={process.env.PUBLIC_URL + '/assets/imgs/home_page_background3.png'} />
+      </div>
     </div>
   );
 };
