86c734722a695876f9ce034ad587f1429ef6ba67
Harrison Lavins <harrisonlavins@oakland.edu>
2020-10-11 15:35:08 -0400

    Added scatterplot load animation

--- visualizations-app/src/graph-builders/scatterplot-builder.js
+++ visualizations-app/src/graph-builders/scatterplot-builder.js
@@ -19,13 +19,15 @@ let svg = d3.select("#graph-container")
       "translate(" + margin.left + "," + margin.top + ")");
 
 
-// Add X axis
+// Add X axis - animation start: 0 opacity
 let x = d3.scaleLinear()
-.domain([4, 8])
+.domain([0, 0])
 .range([ 0, width ]);
 svg.append("g")
+.attr("class", "x-axis")
 .attr("transform", "translate(0," + height + ")")
-.call(d3.axisBottom(x));
+.call(d3.axisBottom(x))
+.attr("opacity", "0");
 
 // Add Y axis
 let y = d3.scaleLinear()
@@ -44,11 +46,29 @@ svg.append('g')
   .attr("cx", d => x(d.x))
   .attr("cy", d => y(d.y))
   .attr("r", 3)
+  //add an id to each point so we can grab it later and modify it
   .attr("id", d => `x:${parseFloat(d.x).toFixed(1)}-y:${parseFloat(d.y).toFixed(1)}`) //id: x:0.0-y:0.0
   //initialize all clusters to black
   .classed("cluster-unassigned",true); 
-}
 
+// Add new X axis - animation end: full opacity, transitioning to correct domain
+x.domain([4, 8])
+svg.select(".x-axis")
+  .transition()
+  .duration(2000)
+  .attr("opacity", "1")
+  .call(d3.axisBottom(x));
+
+//Animate each of the data points to their correct position using different delay
+svg.selectAll("circle")
+  .transition()
+  .delay((d,id) => id*3)
+  .duration(2000)
+  .attr("cx", d => x(d.x))
+  .attr("cy", d => y(d.y))
+
+
+}
 
 
 export default createScatterplot;
