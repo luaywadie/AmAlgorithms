93630e31abbbe5166bb6e0483b7b5dc435150742
Luay Wadie <41401210+luaywadie@users.noreply.github.com>
2020-11-08 12:41:52 -0500

    Formatted script layout

--- visualizations-app/src/Components/algorithms/sorts/SelectionSort.js
+++ visualizations-app/src/Components/algorithms/sorts/SelectionSort.js
@@ -1,13 +1,19 @@
 // Core Imports
-import React, { Component } from 'react';
-import * as d3 from 'd3';
+import React, { Component } from "react";
+import * as d3 from "d3";
 // Styling
-import 'styles/Sorts.scss';
+import "styles/Sorts.scss";
 // Libraries
-import {Container, Row, Tooltip, OverlayTrigger } from 'react-bootstrap';
-import { FaStepBackward, FaStepForward, FaPause, FaPlay,
-        FaPlus, FaMinus, FaSyncAlt} from 'react-icons/fa';
-
+import { Container, Row, Tooltip, OverlayTrigger } from "react-bootstrap";
+import {
+  FaStepBackward,
+  FaStepForward,
+  FaPause,
+  FaPlay,
+  FaPlus,
+  FaMinus,
+  FaSyncAlt,
+} from "react-icons/fa";
 
 class SelectionSort extends Component {
   constructor(props) {
@@ -21,162 +27,146 @@ class SelectionSort extends Component {
       speedChanged: false,
       paused: true,
       swapping: false,
-      interval: null
+      interval: null,
     };
   }
   componentDidMount() {
     // Event Listener to check if window lost focus
     // If so pause the algorithm
     window.onblur = () => {
-      this.setState({paused: true})
-    }
+      this.setState({ paused: true });
+    };
     // Start the Algorithm
     this.restartRandom();
   }
-  
+
   componentWillUnmount() {
     this.endInterval();
   }
 
   startAlgorithm = () => {
-    console.log(this.state.data)
+    console.log(this.state.data);
     // set the dimensions and margins of the graph
     var margin = { top: 20, right: 0, bottom: 0, left: 20 },
-    width = 900 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-    
+      width = 900 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+
     // append the svg object to the body of the page
     var svg = d3
-    .select('#sort-container.selection-sort')
-    .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-    
+      .select("#sort-container.selection-sort")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
     // X axis
     var x = d3
-    .scaleBand()
-    .range([0, width])
-    .domain(this.state.data)
-    .padding(0.2);
-    
+      .scaleBand()
+      .range([0, width])
+      .domain(this.state.data)
+      .padding(0.2);
+
     // Add Y axis
     var y = d3.scaleLinear().domain([0, 10]).range([height, 0]);
-    
+
     // Bars
     svg
-    .selectAll('mybar')
-    .data(this.state.data)
-    .enter()
-    .append('rect')
-    .attr('x', function (d) {
-      return x(d);
-    })
-    .attr('y', function (d) {
-      return y(0);
-    })
-    .attr('width', x.bandwidth())
-    .attr('fill', '#39a4ff')
-    .attr('height', function (d) {
-      return height - y(0);
-    }) // always equal to 0
-    .attr('value', function (d, index) {
-      return index;
-    });
-    
+      .selectAll("mybar")
+      .data(this.state.data)
+      .enter()
+      .append("rect")
+      .attr("x", function (d) {
+        return x(d);
+      })
+      .attr("y", function (d) {
+        return y(0);
+      })
+      .attr("width", x.bandwidth())
+      .attr("fill", "#39a4ff")
+      .attr("height", function (d) {
+        return height - y(0);
+      }) // always equal to 0
+      .attr("value", function (d, index) {
+        return index;
+      });
+
     svg
-    .selectAll('mybar')
-    .data(this.state.data)
-    .enter()
-    .append('text')
-    .attr('x', function (d) {
-      return x(d) + 35;
-    })
-    .attr('y', function (d) {
-      return y(d) - 10;
-    })
-    .attr('value', function (d, index) {
-      return index;
-    })
-    .text(function (d) {
-      return d;
-    });
-    
+      .selectAll("mybar")
+      .data(this.state.data)
+      .enter()
+      .append("text")
+      .attr("x", function (d) {
+        return x(d) + 35;
+      })
+      .attr("y", function (d) {
+        return y(d) - 10;
+      })
+      .attr("value", function (d, index) {
+        return index;
+      })
+      .text(function (d) {
+        return d;
+      });
+
     // Animation
     svg
-    .selectAll('rect')
-    .transition()
-    .duration(800)
-    .attr('y', function (d) {
-      return y(d);
-    })
-    .attr('height', function (d) {
-      return height - y(d);
-    });
-    
+      .selectAll("rect")
+      .transition()
+      .duration(800)
+      .attr("y", function (d) {
+        return y(d);
+      })
+      .attr("height", function (d) {
+        return height - y(d);
+      });
+
     // Calculate Speed
-    this.startInterval()
-  }
+    this.startInterval();
+  };
 
   selectionSort = (arr) => {
-    const len = arr.length;
+    let len = arr.length;
     for (let i = 0; i < len; i++) {
-      let el = arr[i];
-      let j;
-      
-      for (j = i - 1; j >= 0 && arr[j] > el; j--) {
-        arr[j + 1] = arr[j];
-        this.state.animation_queue.push([j, j + 1]);
+      let min = i;
+      for (let j = i + 1; j < len; j++) {
+        if (arr[min] > arr[j]) {
+          min = j;
+        }
       }
-      arr[j + 1] = el;
+      if (min !== i) {
+        let tmp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = tmp;
+        this.state.animation_queue.push([i, min]);
+      }
     }
     return arr;
   };
 
-    selectionSort = (arr) => {
-        let len = arr.length;
-        for (let i = 0; i < len; i++) {
-            let min = i;
-            for (let j = i + 1; j < len; j++) {
-                if (arr[min] > arr[j]) {
-                    min = j;
-                }
-            }
-            if (min !== i) {
-                let tmp = arr[i];
-                arr[i] = arr[min];
-                arr[min] = tmp;
-                this.state.animation_queue.push([i, min]);
-            }
-        }
-        return arr;
-    }
-
   startInterval = () => {
     // Sort
-    this.selectionSort(this.state.data)
-    this.setState({speed: (1000 - ((this.state.speedFactor * 1000) - 1000))})
-    this.setState({interval: this.intervalEngine()})
-  }
+    this.selectionSort(this.state.data);
+    this.setState({ speed: 1000 - (this.state.speedFactor * 1000 - 1000) });
+    this.setState({ interval: this.intervalEngine() });
+  };
 
   restartInterval = () => {
     clearInterval(this.state.interval);
     this.startInterval();
-    this.setState({speedChanged: false})
-  }
+    this.setState({ speedChanged: false });
+  };
 
   endInterval = () => {
     clearInterval(this.state.interval);
-    this.setState({paused: true})
-  }
+    this.setState({ paused: true });
+  };
 
   intervalEngine = () => {
     // Calculate Speed Factor
     let interval = setInterval(() => {
       if (this.state.speedChanged) this.restartInterval();
-      if (this.state.animation_queue.length > 0
-          && !this.state.paused) {
+      if (this.state.animation_queue.length > 0 && !this.state.paused) {
         this.swapBars(
           this.state.animation_queue[0][0],
           this.state.animation_queue[0][1],
@@ -187,85 +177,83 @@ class SelectionSort extends Component {
       } else if (this.state.animation_queue.length == 0) {
         clearInterval(interval);
       } else if (this.state.paused) {
-        console.log("Paused")
+        console.log("Paused");
       }
     }, this.state.speed);
-    
+
     return interval;
-  }
+  };
 
   swapBars(barFromIndex, barToIndex) {
-    this.setState({swapping: true})
+    this.setState({ swapping: true });
     let speed = this.state.speed;
     let fromObj = d3.selectAll("rect[value='" + barFromIndex + "']");
     let toObj = d3.selectAll("rect[value='" + barToIndex + "']");
     let fromObjTxt = d3.selectAll("text[value='" + barFromIndex + "']");
     let toObjTxt = d3.selectAll("text[value='" + barToIndex + "']");
 
-    fromObjTxt
-      .transition()
-      .duration(speed)
-      .attr('x', toObjTxt.attr('x'));
+    fromObjTxt.transition().duration(speed).attr("x", toObjTxt.attr("x"));
 
-    toObjTxt
-      .transition()
-      .duration(speed)
-      .attr('x', fromObjTxt.attr('x'));
+    toObjTxt.transition().duration(speed).attr("x", fromObjTxt.attr("x"));
 
     fromObj
       .transition()
       .duration(speed)
-      .attr('fill', '#9537ff')
-      .attr('x', toObj.attr('x'));
+      .attr("fill", "#9537ff")
+      .attr("x", toObj.attr("x"));
 
     toObj
       .transition()
       .duration(speed)
-      .attr('fill', '#ffa500')
-      .attr('x', fromObj.attr('x'));
+      .attr("fill", "#ffa500")
+      .attr("x", fromObj.attr("x"));
 
     // Reset Colors
-    fromObj.transition().duration(speed).delay(speed).attr('fill', '#39a4ff');
-    toObj.transition().duration(speed).delay(speed).attr('fill', '#39a4ff');
-    
+    fromObj.transition().duration(speed).delay(speed).attr("fill", "#39a4ff");
+    toObj.transition().duration(speed).delay(speed).attr("fill", "#39a4ff");
+
     // Swap
-    let temp = fromObj.attr('value');
-    fromObj.attr('value', toObj.attr('value'));
-    toObj.attr('value', temp);
-    temp = fromObjTxt.attr('value');
-    fromObjTxt.attr('value', toObjTxt.attr('value'));
-    toObjTxt.attr('value', temp);
+    let temp = fromObj.attr("value");
+    fromObj.attr("value", toObj.attr("value"));
+    toObj.attr("value", temp);
+    temp = fromObjTxt.attr("value");
+    fromObjTxt.attr("value", toObjTxt.attr("value"));
+    toObjTxt.attr("value", temp);
 
     // Allow next swapping
     setTimeout(() => {
-      this.setState({swapping: false})
-    }, speed)
+      this.setState({ swapping: false });
+    }, speed);
   }
-  
+
   // Stepper
   stepBack = () => {
     if (this.state.stepper_queue.length > 0) {
-      let toElement = this.state.stepper_queue[this.state.stepper_queue.length - 1][0]
-      let fromElement = this.state.stepper_queue[this.state.stepper_queue.length - 1][1]
-      this.swapBars(toElement, fromElement)
-      this.state.stepper_queue.pop()
-      this.state.animation_queue.unshift([fromElement, toElement])
+      let toElement = this.state.stepper_queue[
+        this.state.stepper_queue.length - 1
+      ][0];
+      let fromElement = this.state.stepper_queue[
+        this.state.stepper_queue.length - 1
+      ][1];
+      this.swapBars(toElement, fromElement);
+      this.state.stepper_queue.pop();
+      this.state.animation_queue.unshift([fromElement, toElement]);
     } else {
       // Handle Button Animations
     }
-  }
+  };
 
   stepForward = () => {
     if (this.state.animation_queue.length > 0) {
-      let toElement = this.state.animation_queue[0][0]
-      let fromElement = this.state.animation_queue[0][1]
-      this.swapBars(toElement, fromElement)
-      this.state.animation_queue.shift()
-      this.state.stepper_queue.unshift([fromElement, toElement])
+      let toElement = this.state.animation_queue[0][0];
+      let fromElement = this.state.animation_queue[0][1];
+      this.swapBars(toElement, fromElement);
+      this.state.animation_queue.shift();
+      this.state.stepper_queue.unshift([fromElement, toElement]);
     } else {
       // Handle Button Animations
     }
-  }
+  };
 
   restartRandom = () => {
     let MIN = 1;
@@ -276,14 +264,13 @@ class SelectionSort extends Component {
       let randomNumber = Math.floor(Math.random() * MAX) + MIN;
       if (randomArray.indexOf(randomNumber) < 0) randomArray.push(randomNumber);
     }
-    
-    this.endInterval()
-    this.clearSVG()
-    this.setState({data: randomArray},
-      () => {
-        this.startAlgorithm()
-      });
-  }
+
+    this.endInterval();
+    this.clearSVG();
+    this.setState({ data: randomArray }, () => {
+      this.startAlgorithm();
+    });
+  };
 
   clearSVG = () => {
     // Clear the document container
@@ -291,8 +278,7 @@ class SelectionSort extends Component {
     // Clear the animation and stepper queues
     this.state.animation_queue = [];
     this.state.stepper_queue = [];
-  }
-
+  };
 
   render() {
     return (
@@ -302,8 +288,12 @@ class SelectionSort extends Component {
             className="graph-button"
             onClick={() => {
               if (this.state.speedFactor >= 0.1) {
-                this.setState({speedChanged: true});
-                this.setState({ speedFactor: parseFloat((this.state.speedFactor - .1).toFixed(1))});
+                this.setState({ speedChanged: true });
+                this.setState({
+                  speedFactor: parseFloat(
+                    (this.state.speedFactor - 0.1).toFixed(1)
+                  ),
+                });
               }
             }}
           >
@@ -316,8 +306,12 @@ class SelectionSort extends Component {
             className="graph-button"
             onClick={() => {
               if (this.state.speedFactor <= 1.4) {
-                this.setState({speedChanged: true});
-                this.setState({ speedFactor: parseFloat((this.state.speedFactor + .1).toFixed(1))});
+                this.setState({ speedChanged: true });
+                this.setState({
+                  speedFactor: parseFloat(
+                    (this.state.speedFactor + 0.1).toFixed(1)
+                  ),
+                });
               }
             }}
           >
@@ -327,9 +321,7 @@ class SelectionSort extends Component {
             key="top"
             placement="top"
             overlay={
-              <Tooltip id="tooltip-top">
-                Pause / Un-Pause Algorithm
-              </Tooltip>
+              <Tooltip id="tooltip-top">Pause / Un-Pause Algorithm</Tooltip>
             }
           >
             <button
@@ -347,7 +339,7 @@ class SelectionSort extends Component {
           <button
             className="graph-button step-back"
             onClick={() => {
-              if (!this.state.swapping) this.stepBack()
+              if (!this.state.swapping) this.stepBack();
             }}
           >
             <FaStepBackward></FaStepBackward>
@@ -355,7 +347,7 @@ class SelectionSort extends Component {
           <button
             className="graph-button btn-label"
             onClick={(e) => {
-              e.preventDefault()
+              e.preventDefault();
             }}
           >
             Stepper
@@ -363,7 +355,7 @@ class SelectionSort extends Component {
           <button
             className="graph-button"
             onClick={() => {
-              if (!this.state.swapping) this.stepForward()
+              if (!this.state.swapping) this.stepForward();
             }}
           >
             <FaStepForward></FaStepForward>
@@ -381,7 +373,7 @@ class SelectionSort extends Component {
               className="graph-button"
               ref="bottom"
               onClick={() => {
-                this.restartRandom()
+                this.restartRandom();
               }}
             >
               <FaSyncAlt></FaSyncAlt>
