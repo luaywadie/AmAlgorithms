8453864c7af749d2233ef5136a4163251b6a2c81
Matt Lanzi <mattlanzi@me.com>
2020-11-16 12:57:29 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AmAlgorithms/precache-manifest.a98cb4c269b6da20de4499c10ea08c18.js"
+  "/AmAlgorithms/precache-manifest.5d40871febdcd863910779c1f6a5bc66.js"
 );
 
 self.addEventListener('message', (event) => {
